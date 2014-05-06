c9d7f628652e3c087e05375dc27af66e3457785e
Berglind <berglindoma13@ru.is>
2014-05-06 15:06:45 +0000

    test commit

--- Screenwriter/Controllers/HomeController.cs
+++ Screenwriter/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Screenwriter.Controllers
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
+			ViewBag.Message = "blabla.Your application description page.";
 
 			return View();
 		}
