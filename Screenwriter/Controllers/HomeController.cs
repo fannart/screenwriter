f7a7b2b44dc317868dd87b288254a922dde98247
Berglind <berglindoma13@ru.is>
2014-05-12 21:43:42 +0000

    views modified

--- Screenwriter/Controllers/HomeController.cs
+++ Screenwriter/Controllers/HomeController.cs
@@ -54,5 +54,9 @@ namespace Screenwriter.Controllers
         {
             return View();
         }
+        public ActionResult Register()
+        {
+            return View();
+        }
 	}
 }
