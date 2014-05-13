9ea0ccb3bd5c0b592a156f8192f8d7e56f10aac9
fannart <fannart@gmail.com>
2014-05-13 17:10:26 +0000

    Database changes

--- Screenwriter/Migrations/Configuration.cs
+++ Screenwriter/Migrations/Configuration.cs
@@ -156,38 +156,41 @@ namespace Screenwriter.Migrations
 				new Entry { ID = 8, SubtitleID = 1, Line1 = "Text", Line2 = "a day keeps the doctor", StartTime = DateTime.Now, Stoptime = DateTime.Now },
 				new Entry { ID = 9, SubtitleID = 1, Line1 = "Text", Line2 = "for my country!", StartTime = DateTime.Now, Stoptime = DateTime.Now }
 			);
-
+			
 			context.Requests.AddOrUpdate(
-				new Request { ID = 1, SubtitleID = 1, UserID = "faade00d-13ef-4042-85d9-dcd28f83cd14" },
-				new Request { ID = 2, SubtitleID = 2, UserID = "faade00d-13ef-4042-85d9-dcd28f83cd14" },
-				new Request { ID = 3, SubtitleID = 3, UserID = "faade00d-13ef-4042-85d9-dcd28f83cd14" },
-				new Request { ID = 4, SubtitleID = 4, UserID = "faade00d-13ef-4042-85d9-dcd28f83cd14" },
-				new Request { ID = 5, SubtitleID = 5, UserID = "faade00d-13ef-4042-85d9-dcd28f83cd14" },
-				new Request { ID = 6, SubtitleID = 6, UserID = "faade00d-13ef-4042-85d9-dcd28f83cd14" },
-				new Request { ID = 7, SubtitleID = 5, UserID = "5d0fee6a-6f8b-45b3-adba-e368c8cb81cc" },
-				new Request { ID = 8, SubtitleID = 6, UserID = "5d0fee6a-6f8b-45b3-adba-e368c8cb81cc" },
-				new Request { ID = 9, SubtitleID = 7, UserID = "5d0fee6a-6f8b-45b3-adba-e368c8cb81cc" },
-				new Request { ID = 10, SubtitleID = 1, UserID = "64958a1c-043f-4168-97e8-22fa51720640" },
-				new Request { ID = 11, SubtitleID = 2, UserID = "64958a1c-043f-4168-97e8-22fa51720640" },
-				new Request { ID = 12, SubtitleID = 3, UserID = "64958a1c-043f-4168-97e8-22fa51720640" },
-				new Request { ID = 13, SubtitleID = 5, UserID = "64958a1c-043f-4168-97e8-22fa51720640" },
-				new Request { ID = 14, SubtitleID = 7, UserID = "64958a1c-043f-4168-97e8-22fa51720640" },
-				new Request { ID = 15, SubtitleID = 3, UserID = "64958a1c-043f-4168-97e8-22fa51720640" },
-				new Request { ID = 16, SubtitleID = 5, UserID = "64958a1c-043f-4168-97e8-22fa51720640" },
-				new Request { ID = 17, SubtitleID = 6, UserID = "64958a1c-043f-4168-97e8-22fa51720640" },
-				new Request { ID = 18, SubtitleID = 6, UserID = "b3ce22ee-b52d-4bb2-9300-6a3a864d838a" },
-				new Request { ID = 19, SubtitleID = 5, UserID = "b3ce22ee-b52d-4bb2-9300-6a3a864d838a" },
-				new Request { ID = 20, SubtitleID = 3, UserID = "b3ce22ee-b52d-4bb2-9300-6a3a864d838a" },
-				new Request { ID = 21, SubtitleID = 6, UserID = "faade00d-13ef-4042-85d9-dcd28f83cd14" },
-				new Request { ID = 22, SubtitleID = 3, UserID = "faade00d-13ef-4042-85d9-dcd28f83cd14" },
-				new Request { ID = 23, SubtitleID = 5, UserID = "faade00d-13ef-4042-85d9-dcd28f83cd14" }
+				new Request { ID = 1, SubtitleID = 1, UserID = "d10c617e-639c-4814-9b46-7fc3f0f0b9ba" },
+				new Request { ID = 2, SubtitleID = 2, UserID = "d10c617e-639c-4814-9b46-7fc3f0f0b9ba" },
+				new Request { ID = 3, SubtitleID = 3, UserID = "d10c617e-639c-4814-9b46-7fc3f0f0b9ba" },
+				new Request { ID = 4, SubtitleID = 4, UserID = "d10c617e-639c-4814-9b46-7fc3f0f0b9ba" },
+				new Request { ID = 5, SubtitleID = 5, UserID = "d10c617e-639c-4814-9b46-7fc3f0f0b9ba" },
+				new Request { ID = 6, SubtitleID = 6, UserID = "d10c617e-639c-4814-9b46-7fc3f0f0b9ba" },
+				new Request { ID = 7, SubtitleID = 7, UserID = "d10c617e-639c-4814-9b46-7fc3f0f0b9ba" },
+				new Request { ID = 8, SubtitleID = 8, UserID = "d10c617e-639c-4814-9b46-7fc3f0f0b9ba" },
+				new Request { ID = 9, SubtitleID = 9, UserID = "d10c617e-639c-4814-9b46-7fc3f0f0b9ba" },
+				new Request { ID = 10, SubtitleID = 5, UserID = "cbbfc2b4-6a3b-4a7d-80be-61e6d2f185e9" },
+				new Request { ID = 11, SubtitleID = 6, UserID = "cbbfc2b4-6a3b-4a7d-80be-61e6d2f185e9" },
+				new Request { ID = 12, SubtitleID = 7, UserID = "cbbfc2b4-6a3b-4a7d-80be-61e6d2f185e9" },
+				new Request { ID = 13, SubtitleID = 1, UserID = "c3c79177-c029-4051-9d57-c2c0c86e8831" },
+				new Request { ID = 14, SubtitleID = 2, UserID = "c3c79177-c029-4051-9d57-c2c0c86e8831" },
+				new Request { ID = 15, SubtitleID = 3, UserID = "c3c79177-c029-4051-9d57-c2c0c86e8831" },
+				new Request { ID = 16, SubtitleID = 5, UserID = "c3c79177-c029-4051-9d57-c2c0c86e8831" },
+				new Request { ID = 17, SubtitleID = 6, UserID = "c3c79177-c029-4051-9d57-c2c0c86e8831" },
+				new Request { ID = 18, SubtitleID = 7, UserID = "c3c79177-c029-4051-9d57-c2c0c86e8831" },
+				new Request { ID = 19, SubtitleID = 8, UserID = "c3c79177-c029-4051-9d57-c2c0c86e8831" },
+				new Request { ID = 20, SubtitleID = 9, UserID = "c3c79177-c029-4051-9d57-c2c0c86e8831" },
+				new Request { ID = 21, SubtitleID = 6, UserID = "a68a9d38-0e2a-42a0-864c-dc706c60500d" },
+				new Request { ID = 22, SubtitleID = 5, UserID = "a68a9d38-0e2a-42a0-864c-dc706c60500d" },
+				new Request { ID = 23, SubtitleID = 3, UserID = "a68a9d38-0e2a-42a0-864c-dc706c60500d" },
+				new Request { ID = 24, SubtitleID = 6, UserID = "95e26a86-113a-4978-9557-39755ab1fef7" },
+				new Request { ID = 25, SubtitleID = 3, UserID = "95e26a86-113a-4978-9557-39755ab1fef7" },
+				new Request { ID = 26, SubtitleID = 5, UserID = "95e26a86-113a-4978-9557-39755ab1fef7" }
 			);
 
 			context.Comments.AddOrUpdate(
-				new Comment() { ID = 1, SubtitleID = 4, TimeStamp = DateTime.Now, UserId = "faade00d-13ef-4042-85d9-dcd28f83cd14", Text = "Geggjað" },
-				new Comment() { ID = 2, SubtitleID = 4, TimeStamp = DateTime.Now, UserId = "faade00d-13ef-4042-85d9-dcd28f83cd14", Text = "WAT!" },
-				new Comment() { ID = 3, EntryID = 1, TimeStamp = DateTime.Now, UserId = "faade00d-13ef-4042-85d9-dcd28f83cd14", Text = "Ekki aftur?" },
-				new Comment() { ID = 4, EntryID = 1, TimeStamp = DateTime.Now, UserId = "faade00d-13ef-4042-85d9-dcd28f83cd14", Text = "No way!" }
+				new Comment() { ID = 1, SubtitleID = 4, TimeStamp = DateTime.Now, UserId = "cbbfc2b4-6a3b-4a7d-80be-61e6d2f185e9", Text = "Geggjað" },
+				new Comment() { ID = 2, SubtitleID = 4, TimeStamp = DateTime.Now, UserId = "cbbfc2b4-6a3b-4a7d-80be-61e6d2f185e9", Text = "WAT!" },
+				new Comment() { ID = 3, EntryID = 1, TimeStamp = DateTime.Now, UserId = "cbbfc2b4-6a3b-4a7d-80be-61e6d2f185e9", Text = "Ekki aftur?" },
+				new Comment() { ID = 4, EntryID = 1, TimeStamp = DateTime.Now, UserId = "cbbfc2b4-6a3b-4a7d-80be-61e6d2f185e9", Text = "No way!" }
 			);
 		}
     }
