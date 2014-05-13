4d54681c1a728075927e7be8061b16ee5b28a75a
fannart <fannart@gmail.com>
2014-05-13 13:01:06 +0000

    The beginning of the Editor Window

---
+++ Screenwriter/Controllers/EditorController.cs
@@ -0,0 +1,27 @@
+﻿using Screenwriter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Screenwriter.Controllers
+{
+    public class EditorController : Controller
+    {
+        //
+        // GET: /Editor/
+        public ActionResult Subtitle(int? id)
+        {
+			if(id.HasValue)
+			{
+				HomeRepository repo = new HomeRepository();
+				int subtitleID = id.Value;
+				
+
+				return View();
+			}
+            return View("Error");
+        }
+	}
+}
---
+++ Screenwriter/ViewModels/EditorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Screenwriter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Screenwriter.ViewModels
+{
+	public class EditorViewModel
+	{
+		Subtitle TranslateFrom { get; set; }
+		Subtitle TranslateTo { get; set; }
+	}
+}
